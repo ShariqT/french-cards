6b63c1fdf7ec17050e240f078c5409bdf818868e
Shariq Torres <shariq.torres@gmail.com>
2025-11-07 00:05:00 -0800

    first

---
+++ src/content.config.js
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+import { file } from 'astro/loaders';
+const ep167 = defineCollection({ 
+  loader: file("src/data/ep167.json"),
+  schema: z.object({
+      id: z.number(),
+      front: z.string(),
+      back: z.string()
+    })
+});
+export const collections = { ep167 };
