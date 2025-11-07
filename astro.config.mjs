490d64933cc0062575e9ccfaded2c5f1c08f22f0
Shariq Torres <shariq.torres@gmail.com>
2025-11-07 00:24:59 -0800

    static in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://shariqt.github.io',
   base: '/french-cards',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
